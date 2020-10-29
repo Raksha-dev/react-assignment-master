995fd79aa605ca4ad6be6534619745c1b7e2a2d9
Raksha@123 <raksha@moonraft.com>
2020-10-29 15:11:18 +0530

    changes

--- src/Common-Components/Spinner/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const spinner = () => (
-    <div></div>
-);
-
-export default spinner;
--- src/axioslogin/axioslogin.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create ( {
-    baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/'
-} )
-export default instance;
-// https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBfd2HjJmstw4eqJzmzOmkOluVKNqs6yZc
